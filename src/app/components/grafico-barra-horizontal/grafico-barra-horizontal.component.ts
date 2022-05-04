bf020ebb73c36a261a6f987f35ac9689c6ce8b7d
Carlos Albarracin <carlos8_alb@hotmail.com>
2022-05-04 19:20:57 -0300

    Added graphics

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent implements OnInit {
+export class GraficoBarraHorizontalComponent {
 
-  constructor() { }
+  results: any[] = [
+    {
+      "name": "Juego 1",
+      "value": 8940000
+    },
+    {
+      "name": "Juego 2",
+      "value": 5000000
+    },
+    {
+      "name": "Juego 3",
+      "value": 7200000
+    }
+  ];
 
-  ngOnInit(): void {
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  yAxisLabel: string = 'Juegos';
+  showYAxisLabel: boolean = true;
+  xAxisLabel: string = 'Votos';
+
+  colorScheme = "nightLights";
+
+  constructor() {
+
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
